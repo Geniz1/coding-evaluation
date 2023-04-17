7653e6378fc773ad8af9dd6969ee4d61187eae9c
Ngene Ugo <egnetic69@gmail.com>
2023-04-17 18:41:45 -0500

    Solution for hire() function

--- dotnet/MyOrganization/Organization.cs
+++ dotnet/MyOrganization/Organization.cs
@@ -26,8 +26,35 @@ namespace MyOrganization
          */
         public Position? Hire(Name person, string title)
         {
-            //your code here
+            // Hashset to keep track of visited nodes while traversing tree
+            var visited = new HashSet<Position>();
+            int id = 0;
+            dfs_traversal(root, visited, title, person, id);
+
             return null;
+        }
+
+        //Implementing recursive solution to traverse tree structure in a depth first search manner
+        //and populate information
+        public void dfs_traversal(Position root, HashSet<Position> visited, string title, Name person, int id)
+        {
+            visited.Add(root);
+            id++; // id is incremented based on hierarchy
+                  // (employees under same boss have similar id with 1 - highest level and 4 - lowest level)
+
+            var reporteees = root.GetDirectReports();
+            if (title == root.GetTitle())
+            {
+                root.SetEmployee(new Employee(id, person));
+            }
+            foreach (var reportee in reporteees)
+            {
+                if (!visited.Contains(reportee))
+                {
+                    // Setting current reportee to root during call
+                    dfs_traversal(reportee, visited, title, person, id);
+                }
+            }
         }
 
         override public string ToString()
